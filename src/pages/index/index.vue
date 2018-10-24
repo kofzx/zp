3c144e47e648b37461d54e71954b70893de7eddd
kofzx <2670476383@qq.com>
2018-10-24 18:17:43 +0800

    zp

---
+++ src/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="index-top container"></div>
+    <!-- <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a> -->
+  </div>
+</template>
+
+<script>
+import wx from '@/utils/wx'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .index-top {
+    height: 46px;
+    background: #ff0000;
+  }
+</style>
